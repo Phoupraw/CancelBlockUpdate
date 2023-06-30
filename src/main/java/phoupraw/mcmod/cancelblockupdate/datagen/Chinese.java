596f233765518cc142b169ef3badbb844e241e90
Phoupraw <467934387@qq.com>
2023-06-30 19:46:47 +0800

    1.0.0-pre5改为1.1.0-pre1

--- src/main/java/phoupraw/mcmod/cancelblockupdate/datagen/Chinese.java
+++ src/main/java/phoupraw/mcmod/cancelblockupdate/datagen/Chinese.java
@@ -20,7 +20,7 @@ public void generateTranslations(TranslationBuilder b) {
     b.add("modmenu.nameTranslation." + CancelBlockUpdate.MOD_ID, modName);
     b.add("modmenu.descriptionTranslation." + CancelBlockUpdate.MOD_ID, "阻止方块更新、方块计划刻、随机刻、放置可行性检查、流体计划刻、铁轨自动转角。在世界生成时启用本模组的效果可能会产生大量浮空方块。可以用游戏规则" + CBUGameRules.OFF.getName() + "开关效果。");
     b.add(CBUGameRules.OFF.getTranslationKey(), modName + "：禁用模组全部效果");
-    b.add(CBUGameRules.REPLACE.getTranslationKey(), modName + "：允许放置方块时替换草和蕨等");
+    b.add(CBUGameRules.REPLACE.getTranslationKey(), modName + "：允许放置方块替换草、蕨");
 }
 
 }
