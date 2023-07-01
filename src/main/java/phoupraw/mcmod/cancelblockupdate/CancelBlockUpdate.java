4eed56af6c00d818454afbbefa3f7c54a26c2600
Phoupraw <467934387@qq.com>
2023-07-01 20:43:56 +0800

    -Dfabric.log.level=DEBUG

--- src/main/java/phoupraw/mcmod/cancelblockupdate/CancelBlockUpdate.java
+++ src/main/java/phoupraw/mcmod/cancelblockupdate/CancelBlockUpdate.java
@@ -1,6 +1,5 @@
 package phoupraw.mcmod.cancelblockupdate;
 
-import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,14 +7,5 @@ public final class CancelBlockUpdate {
 
     public static final String MOD_ID = "cancelblockupdate";
     public static final Logger LOGGER = LogManager.getLogger();
-
-    /**
-     stupid, fooly, shit, hell, damn mojang and log4j
-     */
-    public static void debug(Object msg) {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            LOGGER.info(msg);
-        }
-    }
 
 }
